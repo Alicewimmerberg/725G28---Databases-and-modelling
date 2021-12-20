b8be6352aea7d7722be047cdda2039abe937fc37
hnikih <96429273+hnikih@users.noreply.github.com>
2021-12-20 15:18:13 +0100

    Add files via upload

---
+++ [aliwi837, nikhe458]_DML.sql
@@ -0,0 +1,17 @@
+
+
+
+
+-- Hur många böcker har lånats mellan X och Y?
+
+
+
+-- Hur många lån har en specifik person?
+
+
+
+-- Hur många artifakter har funnits på plats X?
+
+
+
+-- Vilka lån har overdues och hur länge sedan skulle de ha vart återlämnade?
