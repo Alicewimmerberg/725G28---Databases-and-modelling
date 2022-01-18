1cb85e1e4b32d1bdbb78e5a41d053cc3174c0d8e
Alicewimmerberg <71896024+Alicewimmerberg@users.noreply.github.com>
2022-01-18 16:21:46 +0100

    Update [aliwi837, nikhe458]_DML.sql

--- [aliwi837, nikhe458]_DML.sql
+++ [aliwi837, nikhe458]_DML.sql
@@ -1,19 +1,13 @@
-
-
-
 /* How many books has been loaned during year 2021 */
-
 SELECT YEAR (LL.Date_out) AS year,
        COUNT (*) AS Literature_Loans
   FROM Literature_Loans AS LL
  WHERE YEAR (LL.Date_out) = 2021
  GROUP BY LL.Date_out;
  
-
-
 /* How many artifact loans does a specific person have? */
-SELECT P.[Name], 
-       A.Personal_ID, 
+SELECT P.[Name],
+       A.Personal_ID,
        COUNT (P.Personal_ID)  AS 'Amount'
   FROM Person AS P
        INNER JOIN Artifact_Loan AS A
@@ -22,13 +16,11 @@ SELECT P.[Name],
  GROUP BY P.Name, A.Personal_ID
  ORDER BY 'Amount' DESC;
 
-
-
 /* How many artifacts has been found during digs in LKPG? */
-SELECT D.Dig_id, 
-       D.Dig_location, 
+SELECT D.Dig_id,
+       D.Dig_location,
        COUNT (A.Item_Nr) AS 'Amount'
-  FROM Dig AS D 
+  FROM Dig AS D
        INNER JOIN Artifact AS A
        ON D.Dig_id = A.Dig_ID
  WHERE Dig_location ='LKPG'
@@ -36,12 +28,9 @@ SELECT D.Dig_id,
 HAVING COUNT(A.Item_Nr) > 0
  ORDER BY 'Amount' DESC;
 
-
-
-
 /* Which book loans haven't been returned in time? */
-SELECT P.[Name], 
-       L.Overdue, 
+SELECT P.[Name],
+       L.Overdue,
        COUNT (P.Personal_ID)  AS 'Amount'
   FROM Person AS P
        INNER JOIN Literature_Loan AS L
